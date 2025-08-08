33f9c33019c463c2d6caed1baf9b84b2a02931cb
luvcoding22 <102405727+luvcoding22@users.noreply.github.com>
2025-08-08 20:53:17 +0200

    MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,1 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+contract MyToken {
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _supply,
+        address recipient
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _supply * 1e18;
+        _balances[recipient] = totalSupply;
+        emit Transfer(address(0), recipient, totalSupply);
+    }
+
+    function balanceOf(address who) external view returns (uint256) {
+        return _balances[who];
+    }
+
+    function transfer(address to, uint256 value) external returns (bool) {
+        require(_balances[msg.sender] >= value, "Insufficient balance");
+        _balances[msg.sender] -= value;
+        _balances[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(_balances[from] >= amount, "Insufficient balance");
+        require(_allowances[from][msg.sender] >= amount, "Allowance too low");
+
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        _allowances[from][msg.sender] -= amount;
+
+        emit Transfer(from, to, amount);
+        return true;
+    }
+}
